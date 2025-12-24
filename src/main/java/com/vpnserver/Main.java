ad518f02a04e6f3241a21bec984a413f166d27c9
aetos-lang <pushkatank2@gmail.com>
2025-12-24 17:58:44 +0400

    Create Main.java

---
+++ src/main/java/com/vpnserver/Main.java
@@ -0,0 +1,31 @@
+package com.vpnserver;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+public class Main {
+    private static final int PORT = Integer.parseInt(System.getenv().getOrDefault("PORT", "8080"));
+    
+    public static void main(String[] args) throws Exception {
+        System.out.println("Starting VPN Server on port " + PORT);
+        
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(bossGroup, workerGroup)
+             .channel(NioServerSocketChannel.class)
+             .childHandler(new VpnServerInitializer())
+             .childOption(ChannelOption.SO_KEEPALIVE, true);
+            
+            b.bind(PORT).sync().channel().closeFuture().sync();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+}
